c5e6f206074ba075ebdf5102122412d0bde68384
Victor Fernandes <victor.hmfd@gmail.com>
2019-04-21 19:05:14 -0300

    styling profile page
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,21 +1,139 @@
+/* eslint-disable react-native/no-raw-text */
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { UserActions } from '~/store/ducks/user';
 
-// import { Container } from './styles';
+import MultipleCheckBox from '~/components/MultipleCheckBox';
+import { ScrollView } from 'react-native';
 
-export default class Profile extends Component {
-  static navigationOptions = {
-    title: 'Perfil',
+import {
+  Container, Label, Input, Button, ButtonText,
+} from './styles';
+
+class Profile extends Component {
+  state = {
+    username: '' || this.props.username,
+    password: '',
+    passwordConfirmation: '',
+    checkboxes: [
+      {
+        id: 1,
+        title: 'Front-end',
+        checked: false,
+      },
+      {
+        id: 2,
+        title: 'Back-end',
+        checked: false,
+      },
+      {
+        id: 3,
+        title: 'Mobile',
+        checked: false,
+      },
+      {
+        id: 4,
+        title: 'Devops',
+        checked: false,
+      },
+      {
+        id: 5,
+        title: 'Gestão',
+        checked: false,
+      },
+      {
+        id: 6,
+        title: 'Marketing',
+        checked: false,
+      },
+    ],
   };
 
-  state = {};
+  componentDidMount() {
+    this.setPreferences();
+  }
+
+  setPreferences = () => {
+    const { preferences } = this.props;
+    const { checkboxes } = this.state;
+
+    const changedCheckBoxes = checkboxes.map((checkbox) => {
+      preferences.map((preference) => {
+        if (preference.id === checkbox.id) {
+          checkbox.checked = !checkbox.checked;
+        }
+      });
+      return checkbox;
+    });
+
+    this.setState({ checkboxes: changedCheckBoxes });
+  };
+
+  handleCheckBox = (checkboxes) => {
+    this.setState(checkboxes);
+  };
+
+  handleSubmit = () => {
+
+  }
 
   render() {
+    const {
+      username, password, passwordConfirmation, checkboxes,
+    } = this.state;
     return (
-      <View>
-        <Text>Profile</Text>
-      </View>
+      <Container>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Label>Nome</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu nome"
+            underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
+          />
+          <Label>Senha</Label>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Sua senha secreta"
+            underlineColorAndroid="transparent"
+            secureTextEntry
+            value={password}
+            onChangeText={text => this.setState({ password: text })}
+          />
+          <Label>Confirmação de senha</Label>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Sua senha secreta"
+            underlineColorAndroid="transparent"
+            secureTextEntry
+            value={passwordConfirmation}
+            onChangeText={text => this.setState({ passwordConfirmation: text })}
+          />
+          <Label withMargin>Preferências</Label>
+          <MultipleCheckBox checkboxes={checkboxes} handleCheckBox={this.handleCheckBox} />
+          <Button onPress={() => {}}>
+            <ButtonText>Salvar</ButtonText>
+          </Button>
+        </ScrollView>
+      </Container>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  username: state.login.user.username,
+  preferences: state.login.user.preferences,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(UserActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Profile);
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: ${props => props.theme.colors.secondary};
+  flex: 1;
+  justify-content: center;
+  padding: 30px;
+`;
+
+export const Label = styled.Text`
+  font-size: ${props => props.theme.fontSize.small};
+  color: ${props => props.theme.colors.white};
+  font-weight: bold;
+  margin-bottom: ${props => (props.withMargin ? '15px' : '0')};
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  font-size: ${props => props.theme.fontSize.medium};
+  color: ${props => props.theme.colors.white};
+  margin-bottom: 15px;
+  padding-left: 0;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background: ${props => props.theme.colors.primary};
+  border-radius: 50px;
+  padding: 15px;
+  margin-top: 20px;
+`;
+
+export const ButtonText = styled.Text`
+  text-align: center;
+  font-size: ${props => props.theme.fontSize.small};
+  color: ${props => props.theme.colors.white};
+  font-weight: bold;
+`;

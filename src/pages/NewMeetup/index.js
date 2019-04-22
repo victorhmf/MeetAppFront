b04e2672ed775013d8bb0671bd8de3c7100c85fa
Victor Fernandes <victor.hmfd@gmail.com>
2019-04-22 20:30:02 -0300

    styling new meetup page
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- src/pages/NewMeetup/index.js
+++ src/pages/NewMeetup/index.js
@@ -1,22 +1,164 @@
+/* eslint-disable react-native/no-raw-text */
 import React, { Component } from 'react';
-
-import { View, Text } from 'react-native';
-
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { ScrollView, ActivityIndicator } from 'react-native';
+import DatePicker from 'react-native-datepicker';
+import ImagePicker from 'react-native-image-picker';
+import moment from 'moment';
 
-import { Container } from './styles';
+import {
+  Container,
+  Label,
+  Input,
+  Button,
+  ButtonText,
+  Error,
+  ImageButton,
+  StyledImage,
+} from './styles';
+
+import MultipleCheckBox from '~/components/MultipleCheckBox';
 
 export default class NewMeetup extends Component {
   static navigationOptions = {
     tabBarIcon: ({ tintColor }) => <Icon name="plus-square" size={20} color={tintColor} />,
   };
 
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    date: '',
+    imageSource: '',
+    location: '',
+    checkboxes: [
+      {
+        id: 1,
+        title: 'Front-end',
+        checked: false,
+      },
+      {
+        id: 2,
+        title: 'Back-end',
+        checked: false,
+      },
+      {
+        id: 3,
+        title: 'Mobile',
+        checked: false,
+      },
+      {
+        id: 4,
+        title: 'Devops',
+        checked: false,
+      },
+      {
+        id: 5,
+        title: 'Gestão',
+        checked: false,
+      },
+      {
+        id: 6,
+        title: 'Marketing',
+        checked: false,
+      },
+    ],
+  };
+
+  handleCheckBox = (checkboxes) => {
+    this.setState(checkboxes);
+  };
+
+  handleChooseImage = () => {
+    const options = {
+      noData: true,
+    };
+
+    ImagePicker.launchImageLibrary(options, (response) => {
+      const source = { uri: response.uri };
+      this.setState({ imageSource: source });
+    });
+  };
 
   render() {
+    const {
+      title, description, location, date, checkboxes, imageSource,
+    } = this.state;
     return (
       <Container>
-        <Text>NewMeetup</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Label>Título</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o título do meetup"
+            underlineColorAndroid="transparent"
+            value={title}
+            onChangeText={text => this.setState({ title: text })}
+          />
+          <Label>Descrição</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Descreve seu meetup"
+            underlineColorAndroid="transparent"
+            value={description}
+            onChangeText={text => this.setState({ description: text })}
+          />
+
+          <Label>Data</Label>
+          <DatePicker
+            style={{ marginBottom: 15, width: '100%' }}
+            date={date}
+            mode="datetime"
+            placeholder="Selecione a data"
+            format="YYYY-MM-DD HH:mm"
+            minDate={moment().format('YYYY-MM-DD')}
+            maxDate={moment()
+              .add(2, 'years')
+              .format('YYYY-MM-DD')}
+            customStyles={{
+              dateInput: {
+                alignItems: 'flex-start',
+                borderWidth: 0,
+              },
+              placeholderText: {
+                color: '#999',
+                fontSize: 20,
+              },
+              dateText: {
+                color: 'white',
+                fontSize: 20,
+              },
+            }}
+            onDateChange={(text) => {
+              this.setState({ date: text });
+            }}
+          />
+
+          <Label withMargin>Imagem</Label>
+          <ImageButton onPress={this.handleChooseImage}>
+            {imageSource.uri ? (
+              <StyledImage source={imageSource} />
+            ) : (
+              <Icon name="camera" size={20} color="white" />
+            )}
+          </ImageButton>
+
+          <Label>Localização</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Onde seu meetup irá acontecer?"
+            underlineColorAndroid="transparent"
+            value={location}
+            onChangeText={text => this.setState({ location: text })}
+          />
+          <Label withMargin>Tema do meetup</Label>
+          <MultipleCheckBox checkboxes={checkboxes} handleCheckBox={this.handleCheckBox} />
+          <Button>
+            <ButtonText>Salvar</ButtonText>
+          </Button>
+        </ScrollView>
       </Container>
     );
   }
--- src/pages/NewMeetup/styles.js
+++ src/pages/NewMeetup/styles.js
@@ -4,5 +4,57 @@ export const Container = styled.View`
   background: ${props => props.theme.colors.secondary};
   flex: 1;
   justify-content: center;
-  padding: 0px 30px;
+  padding: 30px;
+`;
+
+export const Label = styled.Text`
+  font-size: ${props => props.theme.fontSize.small};
+  color: ${props => props.theme.colors.white};
+  font-weight: bold;
+  margin-bottom: ${props => (props.withMargin ? '15px' : '0')};
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  font-size: ${props => props.theme.fontSize.medium};
+  color: ${props => props.theme.colors.white};
+  margin-bottom: 15px;
+  padding-left: 0;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background: ${props => props.theme.colors.primary};
+  border-radius: 50px;
+  padding: 15px;
+  margin-top: 20px;
+`;
+
+export const ButtonText = styled.Text`
+  text-align: center;
+  font-size: ${props => props.theme.fontSize.small};
+  color: ${props => props.theme.colors.white};
+  font-weight: bold;
+`;
+
+export const Error = styled.Text`
+  font-size: ${props => props.theme.fontSize.small};
+  color: ${props => props.theme.colors.danger};
+`;
+
+export const ImageButton = styled.TouchableOpacity`
+  background: ${props => props.theme.colors.transparent};
+  border-radius: 5px;
+  border: ${props => `1px dashed ${props.theme.colors.regular}`};
+  height: 100px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 15px;
+  overflow: hidden;
+`;
+
+export const StyledImage = styled.Image`
+  height: 100px;
+  width: 100%;
+  border-radius: 5px;
 `;
